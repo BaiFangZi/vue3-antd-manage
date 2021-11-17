3887366325a2996583b7edeeef1ddac0b0bcd25d
weibin <2739666469@qq.com>
2021-11-17 16:51:01 +0800

    chore: 添加git提交规范校验

--- src/views/plugins-demo/markdown.vue
+++ src/views/plugins-demo/markdown.vue
@@ -2,27 +2,28 @@
   <div id="vditor"></div>
 </template>
 <script setup>
-import {  ref, onMounted } from 'vue'
+import { ref, onMounted, markRaw } from 'vue'
 import Vditor from 'vditor'
 import 'vditor/dist/index.css'
 const contentEditor = ref('')
 onMounted(() => {
-  contentEditor.value = new Vditor('vditor', {
-    // height: 360,
-    minHeight: 360,
-    toolbarConfig: {
-      pin: true,
-    },
-    cache: {
-      enable: false,
-    },
-    after: () => {
-      // 异步渲染完成后的回调方法
-      setTimeout(() => {
-         contentEditor.value.setValue('hello,Vditor+Vue!')
-      }, 2000)
-    },
-  })
-
+  contentEditor.value = markRaw(
+    new Vditor('vditor', {
+      // height: 360,
+      minHeight: 360,
+      toolbarConfig: {
+        pin: true,
+      },
+      cache: {
+        enable: false,
+      },
+      after: () => {
+        // 异步渲染完成后的回调方法
+        setTimeout(() => {
+          contentEditor.value.setValue('hello,Vditor+Vue!')
+        }, 2000)
+      },
+    })
+  )
 })
 </script>
