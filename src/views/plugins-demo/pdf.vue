caa446033279871bfd1857bb593e3fded29c3639
weibin <2739666469@qq.com>
2023-07-03 19:58:13 +0800

    feat: 增加html2canvas导出pdf

--- src/views/plugins-demo/pdf.vue
+++ src/views/plugins-demo/pdf.vue
@@ -6,20 +6,23 @@
     <br />
     <a-divider>
       <a-button type="primary" @click="handleWindowPrintPDF"> window.print()生成 </a-button>
-      <a-button style="margin-left: 20px" type="primary" @click="handlePrintJSPDf">
+      <a-button style="margin-left: 20px" type="primary" @click="handlePrintPDF">
         print-js生成
       </a-button>
+      <a-button type="primary" style="margin-left: 20px" @click="handleJsPrintPDF"
+        >js-pdf + html2canvas生成</a-button
+      >
     </a-divider>
-    <h2>目标打印机选择 <span style="color: red"> 另存为PDF</span></h2>
+    <h2>目标打印机选择 <span ref="printDom" style="color: red"> 另存为PDF</span></h2>
     <img
-      width="800"
-      src="https://gitee.com/baifangzi/blogimage/raw/master/img/20210908113732.png"
+      width="200"
+      src="https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg"
       alt=""
     />
 
     <div id="pdf-content">
       <br />
-      <img src="https://gitee.com/baifangzi/blogimage/raw/master/img/20210907193825.png" alt="" />
+      <!-- <img src="https://img.bizhizu.com/2015/1231/20151231030245752.jpg" alt="" /> -->
       <h2>pdf普通文本</h2>
       <p>这是一段文本这是一段文本</p>
       <p>这是一段文本这是一段文本</p>
@@ -34,17 +37,43 @@
   </div>
 </template>
 <script setup>
+import { ref } from 'vue'
 import print from 'print-js'
 
+// vue2版本
+// import html2canvas from '@/plugins/html2canvas.js'
+import html2canvas from 'html2canvas'
+import { jsPDF } from 'jspdf'
 const handleWindowPrintPDF = () => {
   window.print()
 }
-const handlePrintJSPDf = () => {
-  // console.log(23)
+const handlePrintPDF = () => {
+  console.log(23)
   print({
     printable: 'pdf-content',
     type: 'html',
+    useCORS: true, // 解决不显示图片问题
     targetStyles: ['*'], // 处理的样式 ，* 代表所有样式
+  })
+}
+const printDom = ref(null)
+const handleJsPrintPDF = () => {
+  html2canvas(printDom.value, {
+    height: printDom.value.scrollHeight,
+    windowHeight: printDom.value.scrollHeight,
+    useCORS: true, // 解决不显示图片问题
+  }).then((canvas) => {
+    let context = canvas.getContext('2d')
+    let img = new Image()
+    context.drawImage(img, 0, 0, canvas.width, canvas.height)
+    let url = canvas.toDataURL('images/png', 4.0)
+    let imgWidth = 595.28
+    let imgHeight = (595.28 / canvas.width) * canvas.height
+    // debugger
+
+    const pdf = new jsPDF('', 'pt', 'a4')
+    pdf.addImage(url, 'png', 0, 0, imgWidth, imgHeight)
+    pdf.save('维保记录.pdf')
   })
 }
 </script>
--- src/views/realProject/liveVideo/video.vue
+++ src/views/realProject/liveVideo/video.vue
@@ -14,7 +14,9 @@ const playOptions = reactive({
   autoplay: true, // 是否自动播放
   fluid: false, // 自适应宽高
   muted: true,
-  techOrder: ['html5', 'flvjs', 'hlsjs'], // 兼容顺序
+  // techOrder: ['html5', 'flvjs', 'hlsjs'], // 兼容顺序
+  language: 'zh-CN',
+  playbackRates: [1.0, 2.0, 4.0, 8.0], //!this.isMp4 ? [1.0, 2.0, 4.0, 8.
   flvjs: {
     mediaDataSource: {
       isLive: false,
@@ -23,9 +25,13 @@ const playOptions = reactive({
     },
   },
   sources: [
+    // {
+    //   src: props.playUrl,
+    //   type: 'application/x-mpegURL', //this.isMp4 ? 'video/mp4' : 'application/x-mpegURL',
+    // },
     {
       src: props.playUrl,
-      type: 'application/x-mpegURL', //this.isMp4 ? 'video/mp4' : 'application/x-mpegURL',
+      type: 'video/x-flv', //this.isMp4 ? 'video/mp4' : 'application/x-mpegURL',
     },
   ],
 })
@@ -35,6 +41,21 @@ onMounted(() => {
   videoIns.value = videojs(videoRef.value, playOptions, function onPlayerReady() {
     // videoIns.value.src(props.playUrl) //设置url
     videojs.log(`Your player is ready!`)
+    videoIns.value.on('pause', () => {
+      console.log('pause')
+    })
+    videoIns.value.on('playing', () => {
+      console.log('playing')
+    })
+    videoIns.value.on('ratechange', function () {
+      console.log('ratechange', this.playbackRate())
+    })
+    videoIns.value.on('timeupdate', function () {
+      console.log('timeupdate', this.currentTime())
+    })
+    videoIns.value.on('seeked', function () {
+      console.log('seeked', this.currentTime())
+    })
   })
   videojs.addLanguage('zh-CN', videozhCN)
 })
